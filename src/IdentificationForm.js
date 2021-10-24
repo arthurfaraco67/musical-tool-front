d15929b58210a3e305343c29ff56410f86ab110c
Ana Laura de Araujo <anaraujo0847@gmail.com>
2021-10-24 16:14:19 -0300

    Start material ui implementation

--- src/IdentificationForm.js
+++ src/IdentificationForm.js
@@ -1,27 +1,49 @@
-import React, { useEffect } from "react";
-import useInputState from "./hooks/useInputState";
+import React, { useEffect } from 'react';
+import useInputState from './hooks/useInputState';
+import { TextField, Button } from '@mui/material';
 
 export default function IdentificationForm(props) {
-  const [name, updateName] = useInputState("");
-  const [group, updateGroup] = useInputState("");
+	const [ name, updateName ] = useInputState('');
+	const [ group, updateGroup ] = useInputState('');
 
-  useEffect(() => {
-    document.title = `Hello ${name}`;
-  }, [name]);
+	useEffect(
+		() => {
+			document.title = `Hello ${name}`;
+		},
+		[ name ]
+	);
 
-  const handleSubmit = () => {
-    props.updateAnalysis({ name, group });
-    props.nextStep();
-  };
+	const handleSubmit = () => {
+		props.updateAnalysis({ name, group });
+		props.nextStep();
+	};
 
-  return (
-    <div>
-      <h1>
-        Nome: {name} - Grupo: {group}
-      </h1>
-      <input type="text" value={name} onChange={updateName} />
-      <input type="text" value={group} onChange={updateGroup} />
-      <button onClick={handleSubmit}>Próximo</button>
-    </div>
-  );
+	return (
+		<div>
+			<h1>
+				Nome: {name} - Grupo: {group}
+			</h1>
+			<TextField
+				id='name'
+				type='text'
+				value={name}
+				onChange={updateName}
+				label='Nome'
+				variant='standard'
+				color='secondary'
+			/>
+			<TextField
+				id='group'
+				type='text'
+				value={group}
+				onChange={updateGroup}
+				label='Grupo'
+				variant='standard'
+				color='secondary'
+			/>
+			<Button variant='contained' color='secondary' onClick={handleSubmit}>
+				Próximo
+			</Button>
+		</div>
+	);
 }
